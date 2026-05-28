c933bea9b5a9de6ed4933ce38bfcd730bcf572ca
Tim Crook <birdsigh@users.noreply.github.com>
2026-05-28 01:56:59 +0100

    Tend worker for #26: Prompt before bootstrap git pull (#33)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
-git pull
 function doIt() {
+	git pull
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
